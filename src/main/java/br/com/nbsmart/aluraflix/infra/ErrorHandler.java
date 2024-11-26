934571f7e0305d6c1bc578e56675abded3e1d9cb
Nayara Santos <nayy.xd@hotmail.com>
2024-11-26 15:30:53 -0300

    Adding ExceptionHandler class to handle the error 404

---
+++ src/main/java/br/com/nbsmart/aluraflix/infra/ErrorHandler.java
@@ -0,0 +1,15 @@
+package br.com.nbsmart.aluraflix.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity handleError404(){
+        return ResponseEntity.notFound().build();
+    }
+}
